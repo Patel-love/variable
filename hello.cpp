136f032d1b6e6358ac44366eb6a688053fab869e
patel-love <manavamrutiya29@gmail.com>
2024-02-12 15:36:13 +0530

    lecture-3

---
+++ hello.cpp
@@ -0,0 +1,52 @@
+
+// #include <iostream>
+// #define MAX 888
+// using namespace std;
+// int main()
+// {
+//     int a = 5;
+//     cout << a;
+    
+//     float b = 5.8;
+//     cout << b;
+    
+//     bool c = true;
+//     cout << c;
+    
+//     const float b = 6.6;
+//     cout << b;
+    
+//     cout << MAX;
+
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int c;
+    
+    cout << "ENTER THE VALUE OF A :- ";
+    cin >> a;
+    
+    cout << "ENTER THE VALUE OF B :- ";
+    cin >> b;
+    
+    c = a + b;
+    cout << "sum is = " << c <<endl;
+    
+    
+    // cout << "hello" << endl;
+    // cout << "world" ;
+    
+    
+    return 0;
+
+}
+
+
+
